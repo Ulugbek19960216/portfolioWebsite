93c7d08bacaaf007bf5e8e554e6d95eee7671af2
Ulugbek_Leo <sherovulugbek04@gmail.com>
2024-07-01 21:09:40 +0500

    Added basic structure and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.scss";
+
+const App = () => {
+    return (
+        <div>
+            <section><a href="">Hero</a></section>
+            <section>Parallax</section>
+            <section>Services</section>
+            <section>Parallax</section>
+            <section>Portfolio1</section>
+            <section>Portfolio2</section>
+            <section>Portfolio3</section>
+            <section>Contact</section>
+        </div>
+    );
+};
+
+export default App;
